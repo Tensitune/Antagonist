aee0cbd49eb9cd0c97e73768306da205be40a467
Tenshi <tensitune@gmail.com>
2022-09-22 12:23:44 +0500

    Remove gamemode modularity Remove multi-recipient support for notifications

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -10,47 +10,6 @@ GM.Config = GM.Config or {}
 
 Antagonist = Antagonist or {}
 
-if SERVER then
-    AddCSLuaFile("config/config.lua")
-    AddCSLuaFile("libraries/sh_cami.lua")
-end
-
 include("libraries/sh_cami.lua")
 include("config/config.lua")
-
-local SortedPairs = SortedPairs
-local function LoadModules()
-    local root = GM.FolderName .. "/gamemode/modules/"
-    local files, folders = file.Find(root .. "*", "LUA")
-
-    for _, file in ipairs(files) do
-        if string.GetExtensionFromFilename(file) ~= "lua" then continue end
-        include(root .. file)
-    end
-
-    for _, folder in SortedPairs(folders, true) do
-        for _, File in SortedPairs(file.Find(root .. folder .. "/sh_*.lua", "LUA"), true) do
-            if SERVER then
-                AddCSLuaFile(root .. folder .. "/" .. File)
-            end
-
-            include(root .. folder .. "/" .. File)
-        end
-        
-        if SERVER then
-            for _, File in SortedPairs(file.Find(root .. folder .. "/sv_*.lua", "LUA"), true) do
-                include(root .. folder .. "/" .. File)
-            end
-        end
-
-        for _, File in SortedPairs(file.Find(root .. folder .. "/cl_*.lua", "LUA"), true) do
-            if SERVER then
-                AddCSLuaFile(root .. folder .. "/" .. File)
-            else
-                include(root .. folder .. "/" .. File)
-            end
-        end
-    end
-end
-
-LoadModules()
+include("sh_util.lua")
